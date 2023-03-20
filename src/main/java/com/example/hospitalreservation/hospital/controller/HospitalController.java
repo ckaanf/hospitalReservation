5cc948b7f9baed9d849f521c0b6644a87b2f5468
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2023-03-20 19:19:38 +0900

    feature -  병원 도메인 구현
    수정 사항들은 적용하여 병원 도메인 리팩토링

--- src/main/java/com/example/hospitalreservation/hospital/controller/HospitalController.java
+++ src/main/java/com/example/hospitalreservation/hospital/controller/HospitalController.java
@@ -1,9 +1,10 @@
 package com.example.hospitalreservation.hospital.controller;
 
-import com.example.hospitalreservation.hospital.dto.HospitalDto;
 import com.example.hospitalreservation.hospital.dto.response.HospitalResponse;
-import com.example.hospitalreservation.hospital.repository.HospitalRepository;
 import com.example.hospitalreservation.hospital.service.HospitalService;
+import com.example.hospitalreservation.pagination.service.PaginationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,44 +13,77 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/hospitals")
 @Controller
 public class HospitalController {
 
     private final HospitalService hospitalService;
+    private final PaginationService paginationService;
 
-    public HospitalController( HospitalService hospitalService) {
-        this.hospitalService = hospitalService;
-    }
 
     @GetMapping
     public String getHospitalList(
             @RequestParam(required = false) String searchKeyword,
-            @PageableDefault(size =10 , direction = Sort.Direction.DESC)Pageable pageable,
-            ModelMap map
-            ){
-
-        Page<HospitalResponse> hospitals = hospitalService.searchHospitalList(searchKeyword,pageable).map(HospitalResponse::from);
-        map.addAttribute("hospitalList", hospitals);
-
-        return "hospitals/index";
-    }
-
-    @GetMapping("/{hospital-id}")
-    public String getHospital(
-            @PathVariable("hospital-id") Long hospitalId,
+            @PageableDefault(size = 7, direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map) {
 
-        HospitalResponse hospitalDto = HospitalResponse.from(hospitalService.getHospital(hospitalId));
 
-        map.addAttribute("hospital", hospitalDto);
+        Page<HospitalResponse> hospitals = hospitalService.searchHospitalList(searchKeyword, pageable).map(HospitalResponse::from);
+        List<Integer> barNumber = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), hospitals.getTotalPages());
 
-        return "hospitals/detail";
+        log.info("barNumber info : {}", barNumber);
+        log.info("hospitals info : {}", hospitals.getContent());
+
+        map.addAttribute("hospitals", hospitals);
+        map.addAttribute("barNumber", barNumber);
+
+        return "hospital/index";
     }
+
+//    @GetMapping("/{hospital-id}")
+//    public String getHospital(
+//            @PathVariable("hospital-id") Long hospitalId,
+//            ModelMap map) {
+//
+//        HospitalResponse hospitalDto = HospitalResponse.from(hospitalService.getHospital(hospitalId));
+//
+//        map.addAttribute("hospital", hospitalDto);
+//
+//        return "reservation/detail";
+//    }
+
+//    @GetMapping("/{hospital-id}")
+//    public ResponseEntity getHospital(
+//            @PathVariable("hospital-id") Long hospitalId) {
+//        HospitalResponse hospital = HospitalResponse.from(hospitalService.getHospital(hospitalId));
+//
+//        return new ResponseEntity<>(
+//                new SingleResponseDto<>(hospital), HttpStatus.OK
+//        );
+//    }
+
+    @GetMapping("/{hospitalId}")
+    public String hospital(
+            @PathVariable Long hospitalId,
+            ModelMap map){
+
+        HospitalResponse hospital = HospitalResponse.from(hospitalService.getHospital(hospitalId));
+
+        map.addAttribute("hospitalDetail", hospital);
+
+        return "hospital/detail";
+    }
+
+    @ResponseBody
     @GetMapping("/api/load")
-    public String loadHospitalList(){
+    public String loadHospitalList() {
         hospitalService.getHospitalList();
         return null;
     }
 
 }
+
